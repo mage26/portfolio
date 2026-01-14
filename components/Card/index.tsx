b1815316ba88017eebf19e46dc232bdc108a0f77
BenBerlin <bberlin26@gmail.com>
2026-01-13 20:55:23 -0800

    update projects

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -23,7 +23,7 @@ export default function Card({
   example,
 }: Props) {
   return (
-    <div className="flex flex-col md:flex-row min-h-100 w-full justify-between items-center gap-12 bg-section p-8 max-w-6xl rounded-xl">
+    <div className="flex flex-col md:flex-row min-h-100 w-full items-center gap-12 bg-section p-8 max-w-6xl rounded-xl">
       <div
         className="w-60 shrink-0 opacity-0 inview:opacity-100 transition-all duration-700"
         data-inview
@@ -31,7 +31,7 @@ export default function Card({
       >
         <Image src={`/images/${image}`} alt={name} width={240} height={174} />
       </div>
-      <div className="relative flex flex-col gap-4">
+      <div className="relative flex flex-col gap-4 w-full">
         <h3 className="text-3xl font-bold">{name}</h3>
         <div className="flex flex-col md:flex-row gap-2 md:gap-4 md:items-center">
           {position && <h4 className="text-xl">{position}</h4>}
